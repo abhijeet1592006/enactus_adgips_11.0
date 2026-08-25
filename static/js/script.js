89fa178ab810e1c40cf4147f4bcfeb2371b11f7b
abhijeet singh <abhijeet8800434205@gmail.com>
2026-08-25 19:00:29 +0530

    updated_images_and_core_data

--- static/js/script.js
+++ static/js/script.js
@@ -189,8 +189,8 @@ function initTiltEffect() {
             const centerX = rect.width / 2;
             const centerY = rect.height / 2;
 
-            const rotateX = (y - centerY) / 15;
-            const rotateY = (centerX - x) / 15;
+            const rotateX = (y - centerY) / 30;
+            const rotateY = (centerX - x) / 30;
 
             card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale3d(1.02, 1.02, 1.02)`;
         });
@@ -281,29 +281,31 @@ function initGalleryDrag(wrapper) {
 // =============================================
 
 const teamData = {
-    "10.0": {
+    "11.0": {
         council: [
-            { name: "Shruti", role: "President", img: "/static/images/team/Council/1) President.jpg", linkedin: "https://www.linkedin.com/in/shruti-shrivastava-9b16142a1", instagram: "https://www.instagram.com/shrutttiiii.27" },
-            { name: "Shivam Naithani", role: "Vice President", img: "/static/images/team/Council/2) Vice President.jpg", linkedin: "https://www.linkedin.com/in/shivam-naithani-76138a2b8/", instagram: "https://www.instagram.com/naithani_shivam_" },
-            { name: "Kushal Goyal", role: "General Secretary", img: "/static/images/team/Council/3) General Secretory.jpg", pos: "center 5%", linkedin: "https://www.linkedin.com/in/kushalgoyaldev/", instagram: "https://www.instagram.com/kushal_goyal_27/" },
-            { name: "Sujal Bhardwaj", role: "PR Head", img: "/static/images/team/Council/4) PR Head.jpg", linkedin: "https://www.linkedin.com/in/sujal-bhardwaj-8332b92b1/", instagram: "https://www.instagram.com/sujjal.__/" }
+            { name: "Aman", role: "President", img: "/static/images/team/Council/1) President.jpg", linkedin: "#", instagram: "#" },
+            { name: "Priyanshu", role: "Vice President & PR Head", img: "/static/images/team/Council/2) Vice President & PR Head.jpg", linkedin: "#", instagram: "#" },
+            { name: "Ankit", role: "General Secretary", img: "/static/images/team/Council/3) General Secretary.jpg", linkedin: "#", instagram: "#" }
         ],
         department: [
-            { name: "Tushar Jha", role: "Technical Head", img: "/static/images/team/Department heads/1) Technical Head.jpg", linkedin: "https://www.linkedin.com/in/tushar-jha-4350882b1/", instagram: "https://www.instagram.com/tushar_jha._/" },
-            { name: "Ankit Jha", role: "Graphics & Photography Head", img: "/static/images/team/Department heads/2) Graphics & Photography Head.jpg", linkedin: "https://www.linkedin.com/in/ankitt-jha", instagram: "https://www.instagram.com/ankitt_07/" },
-            { name: "Granth Chawla", role: "Graphics & Photography Head", img: "/static/images/team/Department heads/3) Graphics & Photography Head.png", linkedin: "https://www.linkedin.com/in/granth-chawla-489965325/", instagram: "https://www.instagram.com/granthchawla_01/" },
-            { name: "Aman Giri", role: "Research And Content Head", img: "/static/images/team/Department heads/4) Content Head.jpg", linkedin: "https://www.linkedin.com/in/aman-giri-4bb311327", instagram: "https://www.instagram.com/ofc_.aman2g" },
-            { name: "Aniket", role: "Social Media Head", img: "/static/images/team/Department heads/5) Social Media Head.jpg", linkedin: "https://www.linkedin.com/in/", instagram: "https://www.instagram.com/anikethsharmakaushik" },
-            { name: "Radhika", role: "Marketing Head", img: "/static/images/team/Department heads/6) Marketing Head.jpg", linkedin: "https://www.linkedin.com/in/radhika-mittal-0296a8230", instagram: "https://www.instagram.com/enne_radhika/" },
-            { name: "Yug Sharma", role: "Sponsorship Head", img: "/static/images/team/Department heads/7) Sponsorship Head.jpeg", linkedin: "https://www.linkedin.com/in/yug-sharma-1b47b7347/", instagram: "https://www.instagram.com/yug_sharmaa__/" }
+            { name: "Abhijeet Singh", role: "Technical Head", img: "/static/images/team/Department heads/8) Technical Head.jpg", linkedin: "#", instagram: "#", pos: "center 50%", scale: "1" },
+            { name: "Ananya", role: "Graphics & Photography Head", img: "/static/images/team/Department heads/1) Graphics & Photography Head.jpg", linkedin: "#", instagram: "#" },
+            { name: "Suhani", role: "Graphics & Photography Head", img: "/static/images/team/Department heads/2) Graphics & Photography Head.jpg", linkedin: "#", instagram: "#" },
+            { name: "Tripti Ghildiyal", role: "Social Media Head", img: "/static/images/team/Department heads/3) Social Media Head.jpg", linkedin: "#", instagram: "#" },
+            { name: "Namrta", role: "Social Media Head", img: "/static/images/team/Department heads/4) Social Media Head.jpg", linkedin: "#", instagram: "#" },
+            { name: "Bhavik Jain", role: "Marketing & Campaigns Head", img: "/static/images/team/Department heads/5) Marketing & Campaigns Head.jpg", linkedin: "#", instagram: "#" },
+            { name: "Bhavya Bansal", role: "Sponsorship Head", img: "/static/images/team/Department heads/6) Sponsorship Head.jpg", linkedin: "#", instagram: "#" },
+            { name: "AKASH YADAV", role: "Research & Content Head", img: "/static/images/team/Department heads/7) Research & Content Head.jpg", linkedin: "#", instagram: "#" }
         ],
         project: [
-            { name: "Vishv Dhama", role: "Project Navodaya Head", img: "/static/images/team/Project heads/1) Project Navodaya Head.jpg", linkedin: "https://www.linkedin.com/in/vishvdhama1405/", instagram: "https://www.instagram.com/vishv._.dhama/" },
-            { name: "Rubi Negi", role: "Project Navodaya Head", img: "/static/images/team/Project heads/2) Project Navodaya Head.jpg", linkedin: "https://www.linkedin.com/in/rubi-negi28/", instagram: "" },
-            { name: "Lakshay Gupta", role: "Project Astitva Head", img: "/static/images/team/Project heads/3) Project Astitva Head.jpg", linkedin: "https://www.linkedin.com/in/lakshay-gupta-436776349", instagram: "https://www.instagram.com/lakshaayy75" },
-            { name: "S. V. Mounika", role: "Project Astitva Head", img: "/static/images/team/Project heads/4) Project Astitva Head.jpg", linkedin: "https://www.linkedin.com/in/sangati-veera-mounika", instagram: "https://www.instagram.com/mounikareddy8003" },
-            { name: "Priyanshu", role: "Project Vriksh Head", img: "/static/images/team/Project heads/5) Project Vriksh Head.jpg", linkedin: "https://www.linkedin.com/in/priyanshu-thakur-301112326/", instagram: "https://www.instagram.com/pri_yanshu88/" },
-            { name: "Swastiki", role: "Project Vriksh Head", img: "/static/images/team/Project heads/6) Project Vriksh Head.png", linkedin: "https://www.linkedin.com/in/swastiki-vishnoi-871864320", instagram: "https://www.instagram.com/danceamoureuse" }
+            { name: "Rishita Vohra", role: "Project Navodaya Head", img: "/static/images/team/Project heads/1) Project Navodaya Head.jpg", linkedin: "#", instagram: "#" },
+            { name: "Vansh Sharma", role: "Project Navodaya Head", img: "/static/images/team/Project heads/2) Project Navodaya Head.jpg", linkedin: "#", instagram: "#" },
+            { name: "Anamika", role: "Project Vriksh Head", img: "/static/images/team/Project heads/3) Project Vriksh Head.jpg", linkedin: "#", instagram: "#" },
+            { name: "Khushi", role: "Project Vriksh Head", img: "/static/images/team/Project heads/4) Project Vriksh Head.jpg", linkedin: "#", instagram: "#" },
+            { name: "Teena", role: "Project Astitva Head", img: "/static/images/team/Project heads/5) Project Astitva Head.jpg", linkedin: "#", instagram: "#" },
+            { name: "MOHD SAAD", role: "Project Astitva Head", img: "/static/images/team/Project heads/6) Project Astitva Head.jpg", linkedin: "#", instagram: "#" },
+            { name: "Jai", role: "Project Amulya Head", img: "/static/images/team/Project heads/7) Project Amulya Head.jpg", linkedin: "#", instagram: "#" },
+            { name: "Yashaswi", role: "Project Amulya Head", img: "/static/images/team/Project heads/8) Project Amulya Head.jpg", linkedin: "#", instagram: "#" }
         ]
     }
 };
@@ -318,7 +320,8 @@ const legacyData = [
     { tenure: "6", name: "Akashat Dhaunda", designation: "President", achievement: "Introduced Innovation in Social Tech." },
     { tenure: "7", name: "Anushka Khanduja", designation: "President", achievement: "Achieved Strategic Impact Scaling." },
     { tenure: "8", name: "Vibhor Jain", designation: "President", achievement: "National Competition Finalist." },
-    { tenure: "9", name: "Sonika Nautiyal", designation: "President", achievement: "Focused on Sustainable Development Goals." }
+    { tenure: "9", name: "Sonika Nautiyal", designation: "President", achievement: "Focused on Sustainable Development Goals." },
+    { tenure: "10.0", name: "Shruti", designation: "President", achievement: "Led the chapter to new heights of impact." }
 ];
 
 // =============================================
@@ -438,7 +441,7 @@ function renderLegacyTable() {
 // FILTER & NAVIGATION FUNCTIONS
 // =============================================
 
-let currentTenure = "10.0";
+let currentTenure = "11.0";
 let currentCategory = "council";
 
 function filterByTenure() {
